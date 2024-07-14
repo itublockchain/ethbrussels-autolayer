22902e0dee4cb6d001a8e22a4c6deba4d9fe1694
oemerfurkan <omerfurkanyuruk1@gmail.com>
2024-07-14 08:35:10 +0300

    Last i hope

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -10,6 +10,8 @@ const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [orders, setOrders] = useState([]);
   const { writeContractAsync: mockSwapCall } = useScaffoldWriteContract("Automation");
+  const { writeContractAsync: mockSwapCall2 } = useScaffoldWriteContract("MockSwap");
+  const { writeContractAsync: mockSwapCall3 } = useScaffoldWriteContract("MockSwap");
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -26,6 +28,17 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex justify-start items-center w-full flex-col gap-6 p-4 bg-gray-200">
+      <div className="flex gap-4">
+      <button
+        className="w-32 h-16 bg-blue-500 rounded-2xl text-white shadow-xl hover:bg-blue-600"
+        onClick={async () => await mockSwapCall2({ functionName: "sellToken", args: ["0x0177f3a60495B1c55fa0440929C458f5064886c7", BigInt(0.5 * 10 ** 18)] })}
+      >Perform Simulation</button>
+      <button
+        className="w-32 h-16 bg-blue-500 rounded-2xl text-white shadow-xl hover:bg-blue-600"
+        onClick={async () => await mockSwapCall3({ functionName: "sellToken", args: ["0x2Af3E6F7AdB774eEC4EAd62B29FEBAF2929504e2", BigInt(0.5 * 10 ** 18)] })}
+      >Perform Simulation</button>
+      </div>
+      
       <div className="w-full max-w-6xl p-4 rounded flex gap-2">
         <div className="flex gap-4">
           <button className="text-gray-600">All</button>
