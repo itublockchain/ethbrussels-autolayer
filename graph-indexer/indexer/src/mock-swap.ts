08d7ce3c1c6bc4990913187cfae5ba1a4d1bb185
oemerfurkan <omerfurkanyuruk1@gmail.com>
2024-07-14 08:19:30 +0300

    Initialize subgraph

---
+++ graph-indexer/indexer/src/mock-swap.ts
@@ -0,0 +1,48 @@
+import {
+  BuyToken as BuyTokenEvent,
+  Mint as MintEvent,
+  SellToken as SellTokenEvent
+} from "../generated/MockSwap/MockSwap"
+import { BuyToken, Mint, SellToken } from "../generated/schema"
+
+export function handleBuyToken(event: BuyTokenEvent): void {
+  let entity = new BuyToken(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMint(event: MintEvent): void {
+  let entity = new Mint(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleSellToken(event: SellTokenEvent): void {
+  let entity = new SellToken(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
